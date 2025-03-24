0307dfa98008bd16d95fde8ffb3127bf2e5654b1
tahfeezmizan <mizanahmed5316@gmail.com>
2025-03-24 10:08:28 +0600

    Add FAQ management features and improve UI components

--- src/app/(withDashboardLayout)/dashboard/faq/page.tsx
+++ src/app/(withDashboardLayout)/dashboard/faq/page.tsx
@@ -1,12 +1,141 @@
+"use client";
 import FaqCard from "@/components/adminDashboard/pages/faqPage/FaqCard";
 import SectionTitle from "@/components/shared/SectionTitle/SectionTitle";
-import React from "react";
+import Switcher from "@/components/shared/Switcher/Switcher";
+import { Card, Modal } from "antd";
+import React, { useState } from "react";
+import { useForm } from "react-hook-form";
+import { FaRegPenToSquare } from "react-icons/fa6";
 
 const FAQ = () => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [formData, setFormData] = useState({
+    title: "",
+    description: "",
+    bottomText: "",
+  });
+  const [bottomData, setBottomData] = useState({
+    bottomText: "",
+  });
+  
+  type bottomData = any
+
+  const openModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
+
+  const handleInputChange = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+    setBottomData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
+  const { handleSubmit } = useForm<FormData>();
+
+  const onSubmit = (data: bottomData) => {
+    console.log("Form Data:", bottomData);
+    data;
+  };
+
+  const handleButtonSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    console.log(formData);
+    closeModal();
+  };
+
   return (
     <div>
-      <SectionTitle title="FAQ’s" buttonTitle="Add Faq " link="#" />
+      <SectionTitle title="FAQ’s" buttonTitle="Add Faq" onClick={openModal} />
       <FaqCard />
+
+      <div className="mt-6">
+        <Card className="bg-white w-full md:w-[510px]">
+          <form onSubmit={handleSubmit(onSubmit)} className="space-y-4">
+            <div>
+              <div className="flex items-center justify-between mb-3">
+                <label className="block text-[#4E4E4E] text-base font-museomoderno font-semibold uppercase mb-3">
+                  Bottom text :
+                </label>
+                <div className="text-lg">
+                  <FaRegPenToSquare />
+                </div>
+              </div>
+              <input
+                type="text"
+                name="bottomText"
+                value={bottomData.bottomText}
+                onChange={handleInputChange}
+                className="mt-1 px-2 py-3 w-full bg-gray-200/60 rounded-lg"
+                placeholder="Enter title"
+              />
+            </div>
+
+            <div className="flex justify-end">
+              <button className="text-base px-6 md:px-8 py-2 md:py-3 rounded-full font-bold font-museomoderno uppercase bg-gradient-to-r from-[#F9AB7FCC] to-[#FFFFFF]">
+                save
+              </button>
+            </div>
+          </form>
+        </Card>
+      </div>
+
+      <Switcher />
+
+      <Modal open={isModalOpen} onCancel={closeModal} title="" footer={null}>
+        <div className="mb-4">
+          {/* Form fields for title and description */}
+          <div className="w-full flex flex-col gap-6">
+            <div className="w-full mb-4">
+              <label className="block text-[#4E4E4E] text-base font-museomoderno font-semibold uppercase mb-3">
+                TITLE :
+              </label>
+              <input
+                type="text"
+                name="title"
+                value={formData.title}
+                onChange={handleInputChange}
+                className="mt-1 px-2 py-3 w-full bg-gray-200/60 rounded-lg"
+                placeholder="Enter title"
+              />
+            </div>
+
+            <div className="w-full mb-4">
+              <label className="block text-[#4E4E4E] text-base font-museomoderno font-semibold uppercase mb-3">
+                DESCRIPTION :
+              </label>
+              <textarea
+                name="description"
+                value={formData.description}
+                onChange={handleInputChange}
+                className="mt-1 px-2 py-3 w-full bg-gray-200/60 rounded-lg min-h-28"
+                placeholder="Enter description"
+              />
+            </div>
+          </div>
+        </div>
+
+        {/* Submit Button */}
+        <div className="flex justify-center">
+          <button
+            onClick={handleButtonSubmit}
+            className="text-gray300 text-sm md:text-base px-3 md:px-10 py-2 md:py-3 rounded-full font-bold font-museomoderno uppercase bg-gradient-to-r from-[#F9AB7FCC] to-[#FFFFFF]"
+          >
+            Save
+          </button>
+        </div>
+      </Modal>
     </div>
   );
 };
--- src/components/adminDashboard/pages/partnerPage/PartnerCard.tsx
+++ src/components/adminDashboard/pages/partnerPage/PartnerCard.tsx
@@ -1,14 +1,24 @@
-import React from "react";
+"use client";
+import partnerImg from "@/assets/partner-img.png";
+import { UploadOutlined } from "@ant-design/icons";
+import { Button, Card, Input, Modal, Upload } from "antd";
+import Image from "next/image";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
 import { FiEdit } from "react-icons/fi";
 import { RiDeleteBinLine } from "react-icons/ri";
-import partnerImg from "@/assets/partner-img.png";
-import Image from "next/image";
 
 interface Partner {
   image?: File;
   id: number;
   name: string;
   description: string;
+}
+
+interface FormData {
+  title: string;
+  subTitle: string;
+  image?: FileList;
 }
 
 const partners: Partner[] = [
@@ -36,29 +46,110 @@ const partners: Partner[] = [
 ];
 
 export default function PartnerCard() {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const openModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
+
+  const { register, handleSubmit, setValue } = useForm<FormData>();
+
+  const onSubmit = (data: FormData) => {
+    console.log("Form Data:", data);
+  };
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 items-center justify-between text-center gap-6 rounded-md">
-      {partners?.map((item) => (
-        <div key={item?.id} className="bg-[#F7F0E8] p-7 rounded-md text-center">
-          <div className="flex items-end justify-end text-lg gap-2 -mb-5">
-            <FiEdit />
-            <RiDeleteBinLine />
+    <div>
+      <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 items-center justify-between text-center gap-6 rounded-md">
+        {partners?.map((item) => (
+          <div
+            key={item?.id}
+            className="bg-[#F7F0E8] p-7 rounded-md text-center"
+          >
+            <div className="flex items-end justify-end text-lg gap-2 -mb-5">
+              <button onClick={openModal}>
+                <FiEdit />
+              </button>
+              <RiDeleteBinLine />
+            </div>
+            <Image
+              src={partnerImg}
+              width={100}
+              height={100}
+              className="rounded-full mx-auto mb-10"
+              alt={item?.name}
+            />
+            <h2 className="text-2xl font-museomoderno font-semibold mb-2">
+              {item?.name}
+            </h2>
+            <p className="text-base font-light font-poppins">
+              {item?.description}
+            </p>
           </div>
-          <Image
-            src={partnerImg}
-            width={100}
-            height={100}
-            className="rounded-full mx-auto mb-10"
-            alt={item?.name}
-          />
-          <h2 className="text-2xl font-museomoderno font-semibold mb-2">
-            {item?.name}
-          </h2>
-          <p className="text-base font-light font-poppins">
-            {item?.description}
-          </p>
-        </div>
-      ))}
+        ))}
+      </div>
+
+      <div className="">
+        <Modal open={isModalOpen} onCancel={closeModal} title="" footer={null}>
+          <Card className="border-none">
+            <form onSubmit={handleSubmit(onSubmit)} className="space-y-4">
+              <div className="grid grid-cols-1 md:grid-cols-2 gap-10">
+                <div>
+                  <label className="block text-[#4E4E4E] text-base font-museomoderno font-semibold uppercase mb-3">
+                    Title
+                  </label>
+                  <Input
+                    {...register("title")}
+                    placeholder="Enter a title"
+                    className="mb-4 bg-[#E9E9E9] py-3 px-5 outline-none font-poppins"
+                  />
+
+                  <label className="block text-[#4E4E4E] text-base font-museomoderno font-semibold uppercase mb-3">
+                    Sub Title
+                  </label>
+                  <Input
+                    {...register("subTitle")}
+                    placeholder="Enter a sub title"
+                    className="mb-4 bg-[#E9E9E9] py-3 px-5 outline-none font-poppins"
+                  />
+                </div>
+
+                <div className="">
+                  <h3 className="block text-[#4E4E4E] text-base font-museomoderno font-semibold uppercase mb-3">
+                    image
+                  </h3>
+                  <div className="bg-gray-100 flex items-center justify-center  rounded-lg border border-dashed border-gray-300 h-auto">
+                    <Upload
+                      beforeUpload={(file) => {
+                        setValue("image", file as unknown as FileList);
+                        return false;
+                      }}
+                      showUploadList={false}
+                    >
+                      <Button
+                        icon={<UploadOutlined />}
+                        className="!bg-transparent border-0"
+                      >
+                        Upload Image
+                      </Button>
+                    </Upload>
+                  </div>
+
+                  {/* submit button  */}
+                  <div className="flex justify-end mt-6 ">
+                    <button className="text-base px-8 py-3 rounded-full font-bold font-museomoderno uppercase bg-gradient-to-r from-[#F9AB7FCC] to-[#FFFFFF]">
+                      save
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </form>
+          </Card>
+        </Modal>
+      </div>
     </div>
   );
 }
