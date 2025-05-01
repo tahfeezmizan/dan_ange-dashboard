c9acafd5925cf8bbf706328502c9c1f0e3cac13c
tahfeezmizan <mizanahmed5316@gmail.com>
2025-05-01 11:14:36 +0600

    feat: add AdminDashboard, CreateStudent, and UserManagement components

---
+++ src/pages/admin/CreateStudent.tsx
@@ -0,0 +1,4 @@
+
+export default function CreateStudent() {
+  return <div>CreateStudent</div>;
+}
---
+++ src/pages/admin/UserManagement.tsx
@@ -0,0 +1,4 @@
+
+export default function UserManagement() {
+  return <div>UserManagement</div>;
+}
